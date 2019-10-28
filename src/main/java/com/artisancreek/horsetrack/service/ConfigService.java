2b1ae233356562833b7ef835f8d297d4ef7febc9
Mark Wilhelm <mark.wilhelm@artisancreek.com>
2019-10-27 23:06:26 -0500

    Minor refactor to ConfigService

--- src/main/java/com/artisancreek/horsetrack/service/ConfigService.java
+++ src/main/java/com/artisancreek/horsetrack/service/ConfigService.java
@@ -1,16 +1,19 @@
 package com.artisancreek.horsetrack.service;
 
 import com.artisancreek.horsetrack.RaceStatus;
-import com.artisancreek.horsetrack.controller.*;
 import com.artisancreek.horsetrack.model.Horse;
 import com.artisancreek.horsetrack.model.Inventory;
 import com.artisancreek.horsetrack.repository.HorseRepository;
 import com.artisancreek.horsetrack.repository.InventoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConfigService {
+
+  @Value("${max.horses}")
+  private int maxHorses;
 
   @Autowired
   private HorseRepository horseRepository;
@@ -23,6 +26,7 @@ public class ConfigService {
 
   public void loadHorses() {
 
+    // Check value maxHorses for loading correct number of horses
     horseRepository.save(new Horse(1, "That Darn Gray Cat", 5, RaceStatus.WON));
     horseRepository.save(new Horse(2, "Fort Utopia", 10, RaceStatus.LOST));
     horseRepository.save(new Horse(3, "Count Sheep", 9, RaceStatus.LOST));
@@ -41,20 +45,10 @@ public class ConfigService {
     inventoryRepository.save(new Inventory(100,10));
   }
 
-  public void setWinner() {
-
-//    CommandAction commandAction = new CommandActionWinner(new KioskMode(), 1);
-//
-//    CommandActionExecutor commandActionExecutor = new CommandActionExecutor();
-//    commandActionExecutor.executeCommandAction(commandAction);
-
-  }
-
   public void startup() {
 
     loadHorses();
     loadInventory();
-//    setWinner();
 
   }
 
